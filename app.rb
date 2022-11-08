940cc638f267cc5024e6e1ad51ebed679cb33a67
danikdesign <danik.newmail@gmail.com>
2022-11-08 21:56:29 +0200

    Init database, make @db

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 #encoding: utf-8
+require 'sqlite3'
 require 'rubygems'
-require 'sinatra'
+require 'sinatra'v
+
+def init_db
+  @db = SQLite3::Database.new 'blogg.db'
+  @db.results_as_hash = true
+end
+
+before do
+  init_db
+end
+
+configure do
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
@@ -10,3 +23,8 @@ get '/new' do
   erb :new
 end
 
+post '/new' do
+  @message = params[:message]
+
+  erb "You typed #{@message}"
+end
